82f94ac14332ff1ebc17f86b39ed142c934f3669
ravihansa <ravihansad@gmail.com>
2019-08-23 10:18:14 +0530

    Create custom auth user

--- src/customUserAuth/settings.py
+++ src/customUserAuth/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'customUserAuth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'custum_user',
+        'USER': 'root',
+        'PASSWORD': 'mysqlroot',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    },
 }
 
 
--- src/user/views.py
+++ src/user/views.py
@@ -1,3 +1,121 @@
+from rest_framework import generics, mixins, views, status, exceptions
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+from django.http import HttpResponse
 from django.shortcuts import render
+from .serializers import UserSerializer, UserProfileSerializer
+from .models import User
+import json
+import bcrypt
+import ast
 
-# Create your views here.
+
+class UserRegisterView(generics.CreateAPIView):
+    lookup_field = "pk"
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+# class UserUpdateView(generics.RetrieveUpdateDestroyAPIView):
+#     lookup_field = "pk"
+#     serializer_class = UserSerializer
+
+#     def get_queryset(self):
+#         return User.objects.all()
+
+
+class UserLoginView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        if not request.data:
+            return HttpResponse(
+                {"Error": "Please provide your email and password"}, status="400"
+            )
+
+        email = request.data["email"]
+        password = request.data["password"].encode("utf8")
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return HttpResponse(
+                json.dumps({"Error": "Invalid email"}),
+                status=400,
+                content_type="application/json",
+            )
+        if user:
+            usrPassword = ast.literal_eval(user.password)
+            if bcrypt.checkpw(password, usrPassword):
+                token, _ = Token.objects.get_or_create(user=user)
+                return HttpResponse(
+                    json.dumps({"token": token.key}),
+                    status=200,
+                    content_type="application/json",
+                )
+            else:
+                return HttpResponse(
+                    json.dumps({"Error": "Invalid password"}),
+                    status=400,
+                    content_type="application/json",
+                )
+
+
+class UserPasswordChangeView(views.APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        if not request.data:
+            return HttpResponse(
+                {"Error": "Please provide your passwords"}, status="400"
+            )
+
+        email = request.user
+        password = request.data["password"].encode("utf8")
+        newPassword = request.data["new_password"].encode("utf8")
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return HttpResponse(
+                json.dumps({"Error": "Invalid user"}),
+                status=400,
+                content_type="application/json",
+            )
+        if user:
+            userPassword = ast.literal_eval(user.password)
+            if bcrypt.checkpw(password, userPassword):
+                salt = self.saltSecret()
+                hashedPassword = bcrypt.hashpw(newPassword, salt)
+                User.objects.filter(email=email).update(
+                    password=hashedPassword)
+
+                return HttpResponse(
+                    json.dumps({"Sucess": "Password changed successfully"}),
+                    status=200,
+                    content_type="application/json",
+                )
+            else:
+                return HttpResponse(
+                    json.dumps({"Error": "Invalid passowrd"}),
+                    status=400,
+                    content_type="application/json",
+                )
+
+    def saltSecret(self):
+        return bcrypt.gensalt()
+
+
+class UserProfileview(views.APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        user = request.user
+        queryset = User.objects.filter(id=user.id)
+        serializer = UserProfileSerializer(queryset, many=True)
+        user = serializer.data
+        return HttpResponse(
+            json.dumps({"data": user}),
+            status=200,
+            content_type="application/json",
+        )
